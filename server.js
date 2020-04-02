9527c25abe2529cc0fd4db2425ca9325d18c922c
Wagner Lemos <wagnerlduarte@hotmail.com>
2020-04-01 23:42:38 -0300

    cors

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const requireDir = require('require-dir');
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
+// app.use(cors({ origin: 'http://localhost:3000' }));
 
 mongoose.connect('mongodb://192.168.99.100:27017/minhas-series', { useUnifiedTopology: true, useNewUrlParser: true })
 
