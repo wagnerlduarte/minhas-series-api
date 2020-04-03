f4c0b3c42278c443683b351522f4a15e35ba6ff7
Wagner Lemos <wagnerlduarte@hotmail.com>
2020-04-02 21:50:05 -0300

    listen port to deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const requireDir = require('require-dir');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 const app = express();
 app.use(express.json());
@@ -10,10 +10,13 @@ app.use(cors());
 
 // mongoose.connect('mongodb://192.168.99.100:27017/minhas-series', { useUnifiedTopology: true, useNewUrlParser: true })
 
-mongoose.connect('mongodb+srv://series:series@cluster0-mqntg.gcp.mongodb.net/minhas-series?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect(
+  "mongodb+srv://series:series@cluster0-mqntg.gcp.mongodb.net/minhas-series?retryWrites=true&w=majority",
+  { useUnifiedTopology: true, useNewUrlParser: true }
+);
 
-requireDir('./src/models')
+requireDir("./src/models");
 
-app.use('/', require('./src/routes'));
+app.use("/", require("./src/routes"));
 
-app.listen(3001);
+app.listen(process.env.PORT || 3001);
