65c512df1e52c9ceecafcbe4d27f73c9cc2e9a07
Wagner Lemos <wagnerlduarte@hotmail.com>
2020-04-02 21:43:04 -0300

    procfile and mongo connect

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(express.json());
 app.use(cors());
 // app.use(cors({ origin: 'http://localhost:3000' }));
 
-mongoose.connect('mongodb://192.168.99.100:27017/minhas-series', { useUnifiedTopology: true, useNewUrlParser: true })
+// mongoose.connect('mongodb://192.168.99.100:27017/minhas-series', { useUnifiedTopology: true, useNewUrlParser: true })
+
+mongoose.connect('mongodb+srv://series:series@cluster0-mqntg.gcp.mongodb.net/minhas-series?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true })
 
 requireDir('./src/models')
 
