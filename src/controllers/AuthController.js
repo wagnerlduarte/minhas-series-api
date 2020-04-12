7cc81ee5fc66126f1f9a23729529ef8a17f9de4a
Wagner Lemos <wagnerlduarte@hotmail.com>
2020-04-12 20:09:48 -0300

    Integration login

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,39 +1,42 @@
-const mongoose = require("../database");
+const mongoose = require('../database')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model("User");
+const User = mongoose.model('User')
 
 module.exports = {
-  async register(req, res) {
+    async register(req, res) {
+        const { identifier, password } = req.body
 
-    const { identifier, password } = req.body;
+        try {
+            if (await User.findOne({ identifier })) {
+                return res.status(400).send({ error: 'User already exists' })
+            }
 
-    try{
+            const user = await User.create({ identifier, password })
 
-      if(await User.findOne({identifier})){
-        return res.status(400).send({error: 'User already exists'})
-      }
-      
-      const user = await User.create({ identifier, password });
-      
-      user.password = undefined
+            user.password = undefined
 
-      return res.send({ user });
-    }
-    catch(err){
-        return res.status(400).send({error: 'Registration failed: ' + err})
-    }
-  },
+            return res.send({ user })
+        } catch (err) {
+            return res
+                .status(400)
+                .send({ error: 'Registration failed: ' + err })
+        }
+    },
 
-  async login(req, res) {
-    const { identifier, password } = req.body;
+    async login(req, res) {
+        const { identifier, password } = req.body
 
-    if (identifier === "admin@admin.com" && password === "admin") {
-      return res.json({
-        jwt: "d2FnbmVyIGxlbW9zIGR1YXJ0ZQ==",
-        user: identifier,
-      });
-    } else {
-      return res.status(401).send({ error: "Usuário ou senha inválidos" });
-    }
-  },
-};
+        const user = await User.findOne({ identifier }).select('+password')
+
+        if (!user) return res.status(400).send({ error: 'User not found' })
+
+        if (!(await bcrypt.compare(password, user.password)))
+            return res.status(400).send({ error: 'Invalid password' })
+
+        return res.json({
+            jwt: user.password,
+            user: identifier,
+        })
+    },
+}
