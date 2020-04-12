ffa92f9eb7b8d677c95bf74f57dd944d3477b40c
Wagner Lemos <wagnerlduarte@hotmail.com>
2020-04-12 20:43:44 -0300

    Add jwt middleware

--- src/routes/SerieRoutes.js
+++ src/routes/SerieRoutes.js
@@ -1,6 +1,9 @@
 const SerieController = require('../controllers/SerieController')
+const authMiddleware = require('../middlewares/auth')
 
 module.exports = (routes) => {
+    routes.use(authMiddleware)
+
     routes.get('/series', SerieController.list)
     routes.get('/serie/:id', SerieController.get)
     routes.delete('/serie/:id', SerieController.delete)
