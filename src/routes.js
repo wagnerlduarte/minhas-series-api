36c229efe5d9ac06ebae90c89456abf1719373b1
Wagner Lemos <wagnerlduarte@hotmail.com>
2020-04-20 12:04:49 -0300

    remove products feature

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const routes = express.Router()
 
-require('./app/routes/ProductRoutes')(routes)
 require('./app/routes/SerieRoutes')(routes)
 require('./app/routes/AuthRoutes')(routes)
 
